4bd95179502b0d7c9ccc3ad9123128c68c043a68
qs <qs@localhost.localdomain>
2017-12-08 10:12:15 +0800

    update

--- src/main/java/com/unimas/she/presto/functions/UnimasJdbcPushDown.java
+++ src/main/java/com/unimas/she/presto/functions/UnimasJdbcPushDown.java
@@ -1,37 +1,129 @@
 package com.unimas.she.presto.functions;
-
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-
 import com.facebook.presto.spi.function.Description;
 import com.facebook.presto.spi.function.ScalarFunction;
 import com.facebook.presto.spi.function.SqlType;
 import com.facebook.presto.spi.type.StandardTypes;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
 
 public class UnimasJdbcPushDown {
 	
+	
+		
+	private static String getLocation() {
+		String filePath = null;
+		URL url = UnimasJdbcPushDown.class.getProtectionDomain().getCodeSource().getLocation();
+		try {
+			filePath = java.net.URLDecoder.decode(url.getPath(), "utf-8");
+
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return filePath.substring(0, filePath.lastIndexOf("/") + 1);
+	}
+	
+	private static void registerDriver(String jarFile,String className) {
+				
+		File file = new File(jarFile);
+		
+		URL url = null;
+		try {
+			url = file.toURI().toURL();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		
+		URLClassLoader loader =  new URLClassLoader(new URL[]{url});
+		Driver driver = null;
+		
+		try {
+			driver = (Driver) Class.forName(className, true, loader).newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			DriverManager.registerDriver(new DriverShim(driver));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	static {
+		String path = getLocation();
+		registerDriver( path + "postgresql-42.1.4.jar","org.postgresql.Driver");
+	}
+
 	@ScalarFunction("execute_statement")
 	@Description("Returns results")
-	@SqlType(StandardTypes.DOUBLE)
-	public static double statementExecute(@SqlType(StandardTypes.VARCHAR) Slice val) throws SQLException {
-		String url = "jdbc:postgresql://10.68.23.21:5432/las";
+	@SqlType(StandardTypes.VARCHAR)
+	public static Slice statementExecute(@SqlType(StandardTypes.VARCHAR) Slice uri,
+			@SqlType(StandardTypes.VARCHAR) Slice user,
+			@SqlType(StandardTypes.VARCHAR) Slice passwd ,
+			@SqlType(StandardTypes.VARCHAR) Slice sql) throws SQLException {
+		
+		String url = uri.toStringUtf8();
+		
 		Properties properties = new Properties();
-		properties.setProperty("user", "postgres");
-		properties.setProperty("password", "postgres");
+		properties.setProperty("user", user.toStringUtf8());
+		properties.setProperty("password", passwd.toStringUtf8());
+		
 		Connection connection = DriverManager.getConnection(url, properties);
+		
 		Statement statement = connection.createStatement();
-		ResultSet resultSet = statement.executeQuery(val.toStringUtf8());
-		String name = null;
-		while (resultSet.next()) {
-			name = resultSet.getString(1);
-			break;
+		
+		ResultSet resultSet = statement.executeQuery(sql.toStringUtf8());
+		
+		int columnCount = resultSet.getMetaData().getColumnCount();
+		ImmutableList.Builder<String> columns = ImmutableList.builder();
+		
+		for(int i = 0;i < columnCount ;i ++) {
+			columns.add(resultSet.getMetaData().getColumnName(i+ 1));
 		}
-		return Double.valueOf(name);
+		ImmutableList.Builder<Object> results = ImmutableList.builder();
+		
+		while (resultSet.next()) {
+			ImmutableList.Builder<String> row = ImmutableList.builder();
+			for(int i = 0;i < columnCount;i ++) {
+				row.add(resultSet.getString(i + 1));
+			}
+			results.add(row.build());
+		}
+		ImmutableMap.Builder<String, Object> res = ImmutableMap.builder();
+		res.put("MetaData",columns.build());
+		res.put("Results",results.build());
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		String json = null;
+		try {
+			json = mapper.writeValueAsString(res.build());
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		connection.close();
+		return Slices.utf8Slice(json);
 	}
 }
--- src/main/java/com/unimas/she/presto/functions/Utest.java
+++ src/main/java/com/unimas/she/presto/functions/Utest.java
@@ -1,8 +1,14 @@
 package com.unimas.she.presto.functions;
-import static com.unimas.she.presto.functions.UnimasGeoFunctions.*;
-import static com.unimas.she.presto.functions.UnimasStringFunctions.*;
 
+import static com.unimas.she.presto.functions.UnimasGeoFunctions.*;
+import static com.unimas.she.presto.functions.UnimasJdbcPushDown.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,7 +16,13 @@ import java.sql.Statement;
 import java.util.Properties;
 
 import io.airlift.slice.Slices;
+
+
+
+
 public class Utest {
+
+
 	
 	static void bench() {
 		long begin;
@@ -40,29 +52,46 @@ public class Utest {
 		System.out.println("test3--------->" + (end - begin));
 	}
 	
+
 	
-	static void pgTest() throws SQLException {
-		String url = "jdbc:postgresql://10.68.23.21:5432/las";
+	static void pgTest() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
+		
+		File file = new File("/tmp/postgresql-42.1.4.jar");
+		
+		URL url=file.toURI().toURL();  
+		
+		URLClassLoader loader =  new URLClassLoader(new URL[]{url},System.class.getClassLoader());
+		
+		Class<?> cls = loader.loadClass("org.postgresql.Driver");
+
+		Driver driver = (Driver) cls.newInstance();
+		
+		DriverManager.registerDriver(new DriverShim(driver));
+		
+		
+		String jdbcUri = "jdbc:postgresql://10.68.23.21:5432/las";
 		Properties properties = new Properties();
 		properties.setProperty("user", "postgres");
 		properties.setProperty("password", "postgres");
-		Connection connection = DriverManager.getConnection(url, properties);
+		Connection connection = DriverManager.getConnection(jdbcUri, properties);
 
 		Statement statement = connection.createStatement();
-		ResultSet resultSet = statement.executeQuery("select count(1) from zjhm");
+		ResultSet resultSet = statement.executeQuery("select count(1) from zjhm where id > 100");
 		while (resultSet.next()) {
 			String name = resultSet.getString(1);
 			System.out.println(name);
 		}
-
+		loader.close();
 	}
 	
 
-	public static void main(String[] args) throws SQLException {
-//		System.out.println(geoDistance(39.941,116.45,39.94,116.451));
-//		System.out.println(geoDistance2(39.941,116.45,39.94,116.451));
-//		System.out.println(geoDistance3(39.941,116.45,39.94,116.451));
-//		bench();
-		pgTest();
+	public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
+		statementExecute(Slices.utf8Slice("jdbc:postgresql://10.68.23.21:5432/las"),
+				Slices.utf8Slice("postgres"),
+				Slices.utf8Slice("postgres"),
+				Slices.utf8Slice("select * from zjhm where id > 20001 limit 10"));
+
+		
+		
 	}
 }
